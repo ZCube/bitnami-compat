c68ed2c10042a8abc823db1eb7864be491925f45
ZCube <zcube@zcube.kr>
2022-01-28 21:53:01 +0900

    fix rabbitmq

--- scripts/update_repo.sh
+++ scripts/update_repo.sh
@@ -7,12 +7,12 @@ cat $(dirname $1)/prebuildfs/opt/bitnami/.bitnami_components.json | jq 'to_entri
 cat $(dirname $1)/Dockerfile >> _Dockerfile
 cat $(dirname $1)/prebuildfs/opt/bitnami/.bitnami_components.json | jq 'to_entries[] | "\(.key)/\(.value.version)"' | xargs -I {} -n 1 /bin/bash ${ROOT_DIR}/scripts/golang_build_cmd.sh {} > $(dirname $1)/packages.sh
 
-echo "# Install required system packages and dependencies" > _install
-cat $(dirname $1)/prebuildfs/opt/bitnami/.bitnami_components.json | jq 'to_entries[] | "\(.key)/\(.value.version)"' | xargs -I {} -n 1 /bin/bash ${ROOT_DIR}/scripts/docker_installs.sh {} >> _install
+cat $(dirname $1)/prebuildfs/opt/bitnami/.bitnami_components.json | jq 'to_entries[] | "\(.key)/\(.value.version)"' | xargs -I {} -n 1 /bin/bash ${ROOT_DIR}/scripts/docker_installs.sh {} > _install
 echo "COPY --from=golang-builder /opt/bitnami/ /opt/bitnami/" >> _install
+#echo "RUN apt-get update && apt-get upgrade -y && \\" >> _install
 
 sed -i 's/RUN \. \/opt\/bitnami\/scripts\/libcomponent.sh/#RUN . \/opt\/bitnami\/scripts\/libcomponent.sh/g' _Dockerfile 
 
-sed '/# Install required system packages and dependencies/e cat _install' -i _Dockerfile
+sed '/RUN apt-get update && apt-get upgrade -y && \\/e cat _install' -i _Dockerfile
 
 cp -f _Dockerfile $(dirname $1)/Dockerfile
