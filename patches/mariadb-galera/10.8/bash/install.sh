37f530d94819729b4f75a37eb8743330ce52cb99
zcube <zcubekr@gmail.com>
2022-12-24 23:25:57 +0900

    fix: mariadb-galera

--- patches/mariadb-galera/10.8/bash/install.sh
+++ patches/mariadb-galera/10.8/bash/install.sh
@@ -3,7 +3,7 @@
 # over 10.7
 cat <<EOF > rootfs/opt/bitnami/scripts/mariadb-galera/patch.sh
 #!/bin/bash
-sed -i -e "s/wsrep_replicate_myisam=ON/wsrep_mode=REPLICATE_MYISAM/g" '${DB_CONF_DIR}/my.cnf'
+sed -i -e "s/wsrep_replicate_myisam=ON/wsrep_mode=REPLICATE_MYISAM/g" '\${DB_CONF_DIR}/my.cnf'
 EOF
 chmod +x rootfs/opt/bitnami/scripts/mariadb-galera/patch.sh
 sed -i -e "s/export LD_PRELOAD/export LD_PRELOAD;\/opt\/bitnami\/scripts\/mariadb-galera\/patch.sh/g" rootfs/opt/bitnami/scripts/mariadb-galera/run.sh
