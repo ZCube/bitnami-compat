189e514ac175df6e4f774cf8b26a290401444685
zcube <zcubekr@gmail.com>
2023-01-07 17:33:12 +0900

    fix: minio build

--- patches/minio/2023/bash/install.sh
+++ patches/minio/2023/bash/install.sh
@@ -2,6 +2,6 @@
 apt update
 apt install -y wget jq
 echo ${VERSION_MAJOR}-0?${VERSION_MINOR}-0?${VERSION_PATCH}
-export TAG=$(wget -q https://registry.hub.docker.com/v2/repositories/minio/minio/tags -O -  | jq  -r '.results[].name' | grep -E ${VERSION_MAJOR}-0?${VERSION_MINOR}-0?${VERSION_PATCH} | grep -v .fips | sort | tail -n 1)
+export TAG=$(wget -q 'https://registry.hub.docker.com/v2/repositories/minio/minio/tags?page_size=30' -O -  | jq  -r '.results[].name' | grep -E ${VERSION_MAJOR}-0?${VERSION_MINOR}-0?${VERSION_PATCH} | grep -v .fips | sort | tail -n 1)
 echo ${TAG}
 sed -i "s/MINIO_VERSIONSTRING/$TAG/g" Dockerfile.arm64
